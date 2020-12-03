65d0310ad5cbe97c3f66b4fc00d72591a8e9a7ea
serafimdossas <50720098+serafimdossas@users.noreply.github.com>
2020-12-03 14:25:02 +0200

    Added PostActivity and HashtagsActivity

--- app/src/main/java/com/example/twitterpart/TwitterPage.java
+++ app/src/main/java/com/example/twitterpart/TwitterPage.java
@@ -1,13 +1,18 @@
 package com.example.twitterpart;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class TwitterPage extends Activity {
 
+    Button postButton;
+    Button searchButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,5 +22,24 @@ public class TwitterPage extends Activity {
 
         TextView usernameText = findViewById(R.id.usernameText);
         usernameText.setText(username);
+
+
+        postButton = findViewById(R.id.postButton);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent i = new Intent(TwitterPage.this,PostActivity.class);
+                startActivity(i);
+            }
+        });
+
+        searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent i = new Intent(TwitterPage.this,HashtagsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
